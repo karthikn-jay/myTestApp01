9dd8efb508590ce10ff600adba5483ae2ef8fc98
Karthikeyan Jayachandran <karthik.jm85@gmail.com>
2017-02-28 21:12:48 +0530

    Added notification

--- app/src/main/java/com/karthiknjay/mytestapp01/DisplayMessageActivity.java
+++ app/src/main/java/com/karthiknjay/mytestapp01/DisplayMessageActivity.java
@@ -1,8 +1,11 @@
 package com.karthiknjay.mytestapp01;
 
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -22,6 +25,23 @@ public class DisplayMessageActivity extends AppCompatActivity {
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
         layout.addView(textView);
 
+        showNotification();
+    }
+
+    private void showNotification() {
+        try {
+            NotificationCompat.Builder nBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                    .setContentTitle("Test app title")
+                    .setContentText("Test app content")
+                    .setSmallIcon(R.drawable.notification_icon);
+
+            int nId = 101;
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.notify(nId, nBuilder.build());
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d("myTestApp01", e.getMessage());
+        }
     }
 }
 
